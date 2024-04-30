2376a22242be308833bdef2b2fe568a221b38062
jipodine <jipodine@users.noreply.github.com>
2024-05-01 01:49:04 +0200

    dotpi: function to install from git

--- led.js
+++ led.js
@@ -1,35 +1,40 @@
 import { execSync } from 'node:child_process';
 
-const strip_size_default = 3;
+const stripSizeDefault = 3;
 
 let ws281x = null;
 let channel = null;
-let colorsArray = null;
+let coloursArray = null;
+let stripWhiteLed = false;
 
-export function rgb_to_colour(r, g, b) {
-  return (((r << 8) + g) << 8) + b;
+export function rgbw_to_colour(r, g, b, w) {
+  if(stripWhiteLed) {
+    return (((((w << 8) + r) << 8) + g) << 8) + b;
+  } else {
+    return (((r << 8) + g) << 8) + b;
+  }
 }
 
 export function colour_fill(colour) {
-  if (!colorsArray) {
+  if (!coloursArray) {
     return;
   }
   for (let c = 0; c < channel.count; c++) {
-    colorsArray[c] = colour;
+    coloursArray[c] = colour;
   }
 }
 
-export function rgb_fill(r, g, b) {
-  colour_fill(rgb_to_colour(r, g, b));
+export function rgbw_fill(r, g, b, w) {
+  colour_fill(rgbw_to_colour(r, g, b, w));
 }
 
-export function rgb_fill_and_render(r, g, b) {
+export async function rgbw_fill_and_render(r, g, b, w) {
   if (!ws281x) {
     console.error('no ws281x');
     return;
   }
 
-  rgb_fill(r, g, b);
+  rgbw_fill(r, g, b, w);
   ws281x.render();
 }
 
@@ -38,21 +43,18 @@ export function clear() {
     return;
   }
 
-  console.log('clear');
-  rgb_fill(0, 0, 0);
+  rgbw_fill(0, 0, 0, 0);
   ws281x.render();
 }
 
 export async function init({
-  strip_size = strip_size_default,
-  ws281x_options = {
-    dma: 10,
-    freq: 800000,
-    gpio: 12,
-    invert: false,
-    brightness: 255,
-    stripType: 'ws2812',
-  },
+  stripSize = stripSizeDefault,
+  dma = 10,
+  freq = 800000,
+  gpio = 12,
+  invert = false,
+  brightness = 255,
+  stripType = 'ws2812',
 } = {}) {
   let isDotpi = false;
   try {
@@ -62,29 +64,42 @@ export async function init({
     if (isDotpi === 'true') {
       ({ default: ws281x } = await import('rpi-ws281x-native'));
 
-      channel = ws281x(strip_size, ws281x_options);
+      const ws281x_options = {
+        dma,
+        freq,
+        gpio,
+        invert,
+        brightness,
+        stripType,
+      };
+      channel = ws281x(stripSize, ws281x_options);
 
-      // // use channel 1 for PWM1 on GPIO13 PIN33
-      // const channel = ws281x.init({
-      //   dma: 10,
-      //   freq: 800000,
-      //   channels: [
-      //     {count: 0, gpio: 10 },
-      //     {count: strip_size, gpio: 13, invert: false, brightness: 255, stripType: 'ws2812'},
-      //   ]
-      // })[0];
+      stripWhiteLed = false;
+      [
+        ws281x.stripType.SK6812_RGBW,
+        ws281x.stripType.SK6812_RBGW,
+        ws281x.stripType.SK6812_GRBW,
+        ws281x.stripType.SK6812_GBRW,
+        ws281x.stripType.SK6812_BRGW,
+        ws281x.stripType.SK6812_BGRW,
+        ws281x.stripType.SK6812,
+      ].forEach( (stripType) => {
+        stripWhiteLed |= channel.stripType === stripType;
+      });
 
-      colorsArray = channel.array;
+      coloursArray = channel.array;
 
       ['SIGINT', 'SIGTERM', 'SIGHUP'].forEach((signal) => {
         process.on(signal, () => {
           console.log('signal', signal);
           clear();
+          ws281x.reset();
+          ws281x.finalize();
           process.exit();
         });
       });
 
-      rgb_fill_and_render({ r: 0, g: 0,  b: 0 });
+      rgbw_fill_and_render({ r: 0, g: 0, b: 0, w: 0 });
 
     } else {
       isDotpi = false;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import { Server } from '@soundworks/core/server.js';
 import { config } from './config.js';
 
-const server = new Server(config);
+import { program } from 'commander';
+
+import fs from 'node:fs';
+import path from 'node:path';
 
 import * as led from './led.js';
 
-const rgbSchema = {
+const fillSchema = {
   r: {
     type: 'float',
     default: 0,
@@ -24,51 +27,108 @@ const rgbSchema = {
     min: 0,
     max: 255,
   },
+  w: {
+    type: 'float',
+    default: 0,
+    min: 0,
+    max: 255,
+  },
 };
 
-server.stateManager.registerSchema('rgb', rgbSchema);
+const server = new Server(config);
+
+server.stateManager.registerSchema('fill', fillSchema);
+
+// deprecated
+server.stateManager.registerSchema('rgb', fillSchema);
+
+program.option('-C, --configuration <file>', 'LED-strip configuration file');
+
+program.parse();
+const options = program.opts();
+
+
+let ledStripConfigurationFile = options.configuration;
+if(!ledStripConfigurationFile) {
+  // no __dirname in ESM
+  const dirname = path.dirname(new URL(import.meta.url).pathname);
+  ledStripConfigurationFile = path.join(dirname, 'ledstrip-config-default.json');
+  console.log(`No configuration file specified, using default: ${ledStripConfigurationFile}`);
+}
+
+
+const ledStripConfigurationPath = path.resolve(ledStripConfigurationFile);
+const ledStripConfiguration = JSON.parse(fs.readFileSync(ledStripConfigurationPath) );
+console.log('LED strip configuration:', ledStripConfiguration);
 
 await server.start();
-await led.init();
+await led.init(ledStripConfiguration);
 
-led.rgb_fill_and_render(0, 0, 0);
+led.rgbw_fill_and_render(0, 0, 0, 0);
 await new Promise( (resolve) => setTimeout(resolve, 1000) );
 
-const dotpiLedClientCollection = await server.stateManager.getCollection('rgb');
+const fillClientCollection = await server.stateManager.getCollection('fill');
 
-function rgbIntegrate() {
+// deprecated
+const rgbClientCollection = await server.stateManager.getCollection('rgb');
+
+function fillIntegrate() {
 
   const integration = {
     r: 0,
     g: 0,
     b: 0,
+    w: 0,
   };
-  
-  dotpiLedClientCollection.forEach( (client) => {
-    const {r, g, b } = client.getValues();
+
+  const clients = [];
+  fillClientCollection.forEach( (client) => clients.push(client) );
+  // deprecated
+  rgbClientCollection.forEach( (client) => clients.push(client) );
+  clients.forEach( (client) => {
+    const {r, g, b, w } = client.getValues();
     integration.r += r;
     integration.g += g;
     integration.b += b;
+    integration.w += w;
   });
 
   integration.r = Math.max(0, Math.min(255, Math.round(integration.r) ) );
   integration.g = Math.max(0, Math.min(255, Math.round(integration.g) ) ) ;
   integration.b = Math.max(0, Math.min(255, Math.round(integration.b) ) ) ;
+  integration.w = Math.max(0, Math.min(255, Math.round(integration.w) ) );
 
-  const {r, g, b } = integration;
-  led.rgb_fill_and_render(r, g, b);
+  const {r, g, b, w } = integration;
+  led.rgbw_fill_and_render(r, g, b, w);
 }
 
-dotpiLedClientCollection.onAttach((client, updates) => {
-  rgbIntegrate();
+fillClientCollection.onAttach((client, updates) => {
+  fillIntegrate();
 });
 
 
-dotpiLedClientCollection.onUpdate((client, updates) => {
-  rgbIntegrate();
+fillClientCollection.onUpdate((client, updates) => {
+  fillIntegrate();
 });
 
 
-dotpiLedClientCollection.onDetach((client, updates) => {
-  rgbIntegrate();
+fillClientCollection.onDetach((client, updates) => {
+  fillIntegrate();
+});
+
+
+// deprecated
+
+rgbClientCollection.onAttach((client, updates) => {
+  fillIntegrate();
+});
+
+
+rgbClientCollection.onUpdate((client, updates) => {
+  fillIntegrate();
+});
+
+
+rgbClientCollection.onDetach((client, updates) => {
+  fillIntegrate();
 });
