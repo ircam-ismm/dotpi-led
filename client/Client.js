ae367c610fc5418dad58f545738614ce532c8407
jipodine <jipodine@users.noreply.github.com>
2025-02-19 17:56:31 +0100

    client: allow for dummy client on non-dotpi system

--- client/Client.js
+++ client/Client.js
@@ -1,4 +1,6 @@
 import { Client as SoundworksClient } from '@soundworks/core/client.js';
+import { isRaspberryPi } from '@dotpi/javascript/system.js';
+import * as echo from '@dotpi/javascript/echo.js';
 import * as configuration from './configuration.js';
 
 export class Client {
@@ -13,23 +15,38 @@ export class Client {
   constructor({
     configurationFile,
   } = {}) {
+    this.isRaspberryPi = null;
     this.configurationFile = configurationFile;
+    this.configuration = null;
     this.soundworksClient = null;
-    this.configuration = {};
   }
 
   async init() {
-    this.configuration = await configuration.read(this.configurationFile);
-    this.configurationSoundworks = {
-      ...configuration.soundworksClient,
-    };
-    Object.assign(this.configurationSoundworks.env, this.configuration.server);
+    this.isRaspberryPi = await isRaspberryPi();
 
-    this.soundworksClient = new SoundworksClient(this.configurationSoundworks);
+    try {
+      this.configuration = await configuration.read(this.configurationFile);
+      this.configurationSoundworks = {
+        ...configuration.soundworksClient,
+      };
+      Object.assign(this.configurationSoundworks.env, this.configuration.server);
+
+      this.soundworksClient = new SoundworksClient(this.configurationSoundworks);
+    } catch (error) {
+      if (!this.isRaspberryPi) {
+        echo.warning('Not a dotpi system, and no valid configuration file: dummy client');
+        console.warn(`(${error.message})`);
+      } else {
+        throw error;
+      }
+    }
     return this;
   }
 
   async start() {
+    if (!this.configuration) {
+      return this;
+    }
     await this.soundworksClient.start();
     this.fillState = await this.soundworksClient.stateManager.create('fill');
 
@@ -42,6 +59,9 @@ export class Client {
     b = 0,
     w = 0,
   } = {}) {
+    if (!this.configuration) {
+      return;
+    }
     await this.fillState.set({ r, g, b, w });
   }
 
